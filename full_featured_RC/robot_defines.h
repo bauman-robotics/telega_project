5eb15c937c0faae8bdbb2b23b70b7ba46771e20a
Notgnoshi <notgnoshi@gmail.com>
2016-02-20 20:17:32 +0000

    full featured RC code

---
+++ full_featured_RC/robot_defines.h
@@ -0,0 +1,16 @@
+#ifndef __ROBOT_DEFINES_
+#define __ROBOT_DEFINES_
+
+
+// motor defines
+#define LEFT_MOTOR_FLAG  0x4C
+#define RIGHT_MOTOR_FLAG 0x52
+#define SERVO_CLAW_CLOSE_TAG  0x41
+#define SERVO_CLAW_RAISE_TAG  0x42
+
+//communication defines
+#define ROBOT_BAUDRATE 115200
+#define ARDUINO_COMM_LOCATION "/dev/robot/arduino"
+#define ARDUINO_RECEIVED_BYTE 0x13
+
+#endif
